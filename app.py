fe4bb538b7d44fb3ce7d5270d871ffa4e6c74b26
nodelmonster <tom.keilers@icloud.com>
2023-09-16 19:11:59 +0200

    deleted unnecessary file, added new layout file

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html", title="Home", language="en")
+@app.route("/en")
+def en():
+    return render_template("layout_new.html", language="en")
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/de")
+def de():
+    return render_template("home.html", language="de")
+
+@app.route("/jp")
+def jp():
+    return render_template("home.html", language="jp")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
